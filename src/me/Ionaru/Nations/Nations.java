15dcc7407887175e8c970b482a35106e44a331db
Ionaru <Jeroen_akkerman1@hotmail.com>
2014-05-04 15:10:01 +0200

    Improved messages

--- src/me/Ionaru/Nations/Nations.java
+++ src/me/Ionaru/Nations/Nations.java
@@ -34,13 +34,14 @@ public class Nations extends JavaPlugin{
 		if(commandLabel.equalsIgnoreCase("nation") || commandLabel.equalsIgnoreCase("n") || commandLabel.equalsIgnoreCase("nations")){
 			if(args.length == 0){
 				if(player.isOp()){
-					player.sendMessage("----" + ChatColor.GREEN + "Nations Help (Admin)" + ChatColor.WHITE + "---- " + Nation[1]);
-					player.sendMessage(ChatColor.AQUA + "/n, /nation and /nations");
+					player.sendMessage("----" + ChatColor.GREEN + " Nations Help (Admin) " + ChatColor.WHITE + "----");
+					player.sendMessage(ChatColor.AQUA + "Accepted command aliases: /n, /nation and /nations");
 					player.sendMessage(ChatColor.AQUA + "/Nation" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Shows info on the Nations plugin commands.");
 					player.sendMessage(ChatColor.AQUA + "/Nation list" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays active nations.");
 					player.sendMessage(ChatColor.AQUA + "/Nation info <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays additional info on a nation.");
 					player.sendMessage(ChatColor.AQUA + "/Nation join <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Join a nation.");
-				}else{
+				}
+				else{
 					player.sendMessage("----" + ChatColor.GREEN + "Nations Help" + ChatColor.WHITE + "----");
 					player.sendMessage(ChatColor.AQUA + "/Nation" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Shows info on the Nations plugin commands.");
 					player.sendMessage(ChatColor.AQUA + "/Nation list" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays active nations.");
@@ -51,27 +52,56 @@ public class Nations extends JavaPlugin{
 			else if(args.length == 1){
 				if(args[0].equals("?") || args[0].equalsIgnoreCase("help")){
 					if(player.isOp()){
-						player.sendMessage("----" + ChatColor.GREEN + "Nations Help (Admin)" + ChatColor.WHITE + "----");
-						player.sendMessage(ChatColor.AQUA + "/n, /nation and /nations");
+						player.sendMessage("----" + ChatColor.GREEN + " Nations help (Admin) " + ChatColor.WHITE + "----");
+						player.sendMessage(ChatColor.AQUA + "Accepted command aliases: /n, /nation and /nations");
 						player.sendMessage(ChatColor.AQUA + "/Nation" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Shows info on the Nations plugin commands.");
 						player.sendMessage(ChatColor.AQUA + "/Nation list" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays active nations.");
 						player.sendMessage(ChatColor.AQUA + "/Nation info <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays additional info on a nation.");
 						player.sendMessage(ChatColor.AQUA + "/Nation join <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Join a nation.");
-					}else{
-						player.sendMessage("----" + ChatColor.GREEN + "Nations Help" + ChatColor.WHITE + "----");
+					}
+					else{
+						player.sendMessage("----" + ChatColor.GREEN + " Nations Help " + ChatColor.WHITE + "----");
 						player.sendMessage(ChatColor.AQUA + "/Nation" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Shows info on the Nations plugin commands.");
 						player.sendMessage(ChatColor.AQUA + "/Nation list" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays active nations.");
 						player.sendMessage(ChatColor.AQUA + "/Nation info <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays additional info on a nation.");
 						player.sendMessage(ChatColor.AQUA + "/Nation join <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Join a nation.");
 					}
 				}
+				else if(args[0].equalsIgnoreCase("list")){
+					int counter = 0;
+					player.sendMessage("----" + ChatColor.GREEN + " Active nations " + ChatColor.WHITE + "----");
+					player.sendMessage(ChatColor.AQUA + Nation[counter] + ChatColor.WHITE + " | " + ChatColor.GREEN + "Use " + ChatColor.GOLD + "/nation info " + Nation[counter] + ChatColor.GREEN + " for more information.");
+					counter++;
+					player.sendMessage(ChatColor.AQUA + Nation[counter] + ChatColor.WHITE + " | " + ChatColor.GREEN + "Use " + ChatColor.GOLD + "/nation info " + Nation[counter] + ChatColor.GREEN + " for more information.");
+					counter++;
+					player.sendMessage(ChatColor.AQUA + Nation[counter] + ChatColor.WHITE + " | " + ChatColor.GREEN + "Use " + ChatColor.GOLD + "/nation info " + Nation[counter] + ChatColor.GREEN + " for more information.");
+					counter++;
+					player.sendMessage(ChatColor.AQUA + Nation[counter] + ChatColor.WHITE + " | " + ChatColor.GREEN + "Use " + ChatColor.GOLD + "/nation info " + Nation[counter] + ChatColor.GREEN + " for more information.");
+				}
 				else if(args[0].equalsIgnoreCase("info") || args[0].equalsIgnoreCase("join")){
 					player.sendMessage(ChatColor.RED + "Invalid arguments, please choose a nation.");
 				}
 			}
-			else if(args.length ==2){
-				if(args[0].equalsIgnoreCase("info") && args[1].equalsIgnoreCase(Nation[1])){
-					player.sendMessage(ChatColor.AQUA + "The Netherlands" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<Info on The Netherlands here...>");
+			else if(args.length == 2){
+				if(args[0].equalsIgnoreCase("info") && args[1].equalsIgnoreCase(Nation[0])){
+					player.sendMessage("----" + ChatColor.GREEN + " Nation info " + ChatColor.AQUA + "(England) " + ChatColor.WHITE + "----");
+					player.sendMessage(ChatColor.AQUA + "General info" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<General info on England here...>");
+					player.sendMessage(ChatColor.AQUA + "Traits" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<Info on English traits here...>");
+				}
+				else if(args[0].equalsIgnoreCase("info") && args[1].equalsIgnoreCase(Nation[1])){
+					player.sendMessage("----" + ChatColor.GREEN + " Nation info " + ChatColor.AQUA + "(The Netherlands) " + ChatColor.WHITE + "----");
+					player.sendMessage(ChatColor.AQUA + "General info" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<General info on The Netherlands here...>");
+					player.sendMessage(ChatColor.AQUA + "Traits" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<Info on Dutch traits here...>");
+					}
+				else if(args[0].equalsIgnoreCase("info") && args[1].equalsIgnoreCase(Nation[2])){
+					player.sendMessage("----" + ChatColor.GREEN + " Nation info " + ChatColor.AQUA + "(Spain) " + ChatColor.WHITE + "----");
+					player.sendMessage(ChatColor.AQUA + "General info" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<General info on Spain here...>");
+					player.sendMessage(ChatColor.AQUA + "Traits" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<Info on Spanish traits here...>");
+				}
+				else if(args[0].equalsIgnoreCase("info") && args[1].equalsIgnoreCase(Nation[3])){
+					player.sendMessage("----" + ChatColor.GREEN + " Nation info " + ChatColor.AQUA + "(France) " + ChatColor.WHITE + "----");
+					player.sendMessage(ChatColor.AQUA + "General info" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<General info on France here...>");
+					player.sendMessage(ChatColor.AQUA + "Traits" + ChatColor.WHITE + " | " + ChatColor.GREEN + "<Info on French traits here...>");
 				}
 			}
 		}
