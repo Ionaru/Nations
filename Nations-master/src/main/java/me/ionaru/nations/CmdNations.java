f556b2f4b82c7b41537cc81e45def442857f822b
mythbusterma <wiirocksme@gmail.com>
2014-05-18 20:23:12 -0400

    Refactored some code, removed "Nation" class

---
+++ Nations-master/src/main/java/me/ionaru/nations/CmdNations.java
@@ -0,0 +1,133 @@
+package me.ionaru.nations;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CmdNations implements CommandExecutor {
+
+    private Nations parent;
+
+    public CmdNations(Nations plugin){
+        this.parent = plugin;
+    }
+
+    public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args){
+        if(cmd.getName().equalsIgnoreCase("nations")){
+            if(args.length == 0){
+                if(cs.hasPermission("nations.admin")){
+                    return adminHelp(cs);
+                }else{
+                    return userHelp(cs);
+                }
+            } else if(args.length == 1){
+                if(args[0].equalsIgnoreCase("help") || args[0].equals("?"))  {
+                    if(cs.hasPermission("nations.admin")) {
+                        return adminHelp(cs);
+                    } else{
+                        return userHelp(cs);
+                    }
+                } else if(args[0].equalsIgnoreCase("list") || args[0].equalsIgnoreCase("info")){
+                    return nationList(cs);
+                } else if(args[0].equalsIgnoreCase("join")){
+                    return invalidParameters(cs);
+                } else if(args[0].equalsIgnoreCase("leave")){
+                    return nationLeave(cs);
+                } else{
+                	return nationErrorCommand(cs);
+                }
+            } else if(args.length == 2) {
+                if (args[0].equalsIgnoreCase("info")) {
+                    return nationInfo(cs, args);
+                } else if(args[0].equalsIgnoreCase("join")){
+                    return nationJoin(cs, args);
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean adminHelp(CommandSender cs){
+        cs.sendMessage("----" + ChatColor.AQUA + " Nations Help (" + ChatColor.RED + "Admin" +  ChatColor.GREEN + ") " + ChatColor.WHITE + "----");
+        cs.sendMessage(ChatColor.GREEN + "Accepted command aliases: " + ChatColor.GOLD + "/n" + ChatColor.GREEN + "," + ChatColor.GOLD + " /nation" + ChatColor.GREEN + " and" + ChatColor.GOLD + " /nations");
+        cs.sendMessage(ChatColor.GOLD + "/Nation" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Shows info on the Nations plugin commands.");
+        cs.sendMessage(ChatColor.GOLD + "/Nation list" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays active nations.");
+        cs.sendMessage(ChatColor.GOLD + "/Nation info <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays additional info on a nation.");
+        cs.sendMessage(ChatColor.GOLD + "/Nation join <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Join a nation.");
+        return true;
+    }
+
+    private boolean userHelp(CommandSender cs){
+        cs.sendMessage("----" + ChatColor.AQUA + " Nations Help " + ChatColor.WHITE + "----");
+        cs.sendMessage(ChatColor.GREEN + "Accepted command aliases: " + ChatColor.GOLD + "/n" + ChatColor.GREEN + "," + ChatColor.GOLD + " /nation" + ChatColor.GREEN + " and" + ChatColor.GOLD + " /nations");
+        cs.sendMessage(ChatColor.GOLD + "/Nation" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Shows info on the Nations plugin commands.");
+        cs.sendMessage(ChatColor.GOLD + "/Nation list" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays active nations.");
+        cs.sendMessage(ChatColor.GOLD + "/Nation info <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Displays additional info on a nation.");
+        cs.sendMessage(ChatColor.GOLD + "/Nation join <Nation name>" + ChatColor.WHITE + " | " + ChatColor.GREEN + "Join a nation.");
+        return true;
+    }
+
+    private boolean invalidParameters(CommandSender cs){
+        cs.sendMessage(ChatColor.RED + "Invalid arguments, please choose a nation.");
+        return true;
+    }
+
+    private boolean nationList(CommandSender cs){
+        cs.sendMessage(parent.colorize("&f---- &bActive nations &f----"));
+        for(NationType n: NationType.values()){
+            cs.sendMessage(parent.colorize("&b" + n.getTitle() + "&f | &aUse &6/nation info " + n.getTitle() + "&a for more information."));
+        }
+        return true;
+    }
+
+    private boolean nationLeave(CommandSender cs){
+        if(isConsole(cs)){
+            return true;
+        }
+        parent.removeFromNation((Player)cs);
+        return true;
+    }
+
+    private boolean nationInfo(CommandSender cs, String[] args){
+        String nation = args[1].toUpperCase();
+        if(!NationType.contains(nation)){
+        	cs.sendMessage(parent.colorize("&cThat nation does not exist, use &6/nation list &cto see the active nations!"));
+            return true;
+        }
+        NationType type = NationType.valueOf(nation);
+        cs.sendMessage(parent.colorize("&f---- &6Nation info: &b" + type.getTitle() + " &f----"));
+        cs.sendMessage(parent.colorize("&6Lore: &a" + type.getLore()));
+        cs.sendMessage(parent.colorize("----"));
+        cs.sendMessage(parent.colorize("&6Traits: &a" + type.getTraits()));
+        cs.sendMessage(parent.colorize("----"));
+        return true;
+    }
+
+    private boolean nationJoin(CommandSender cs, String[] args){
+        String nation = args[1].toUpperCase();
+        if(isConsole(cs)){
+            return true;
+        }
+        if(!NationType.contains(nation)){
+        	cs.sendMessage(parent.colorize("&cThat nation does not exist, use &6/nation list &cto see the active nations!"));
+            return true;
+        }
+        parent.addToNation(NationType.valueOf(nation), (Player) cs);
+        return true;
+    }
+
+    private boolean isConsole(CommandSender cs){
+        if (!(cs instanceof Player)) {
+            cs.sendMessage(Nations.colorize("&4This command is only available to players!"));
+            return true;
+        }
+        return false;
+    }
+    
+    private boolean nationErrorCommand(CommandSender cs){
+    	cs.sendMessage(Nations.colorize("&cThis is not a valid Nations command!"));
+		return true;
+    }
+}
---
+++ Nations-master/src/main/java/me/ionaru/nations/NationType.java
@@ -0,0 +1,46 @@
+package me.ionaru.nations;
+
+public enum NationType {
+    ENGLAND(3,"England", "England had a number of colonies in America with Jamestown as the first permanent colony on American soil. Established in the marches of current-day Virginia on May 14, 1607 by the Virginia Company. The town was named after king James I, who ruled over Scotland and England until 1625. \nEngland later captured large parts of East-America in the Anglo-Dutch wars.", "Chance to do extra damage with bow & arrow."),
+    NETHERLANDS(2,"Netherlands", "The Netherlands colonized large parts of America's east coast during the 17th century, also known as the Dutch Golden Age. \nNew-Netherland was founded in 1614 with New-Amsterdam as it's capital. New Amsterdam was later captured by the English in the Anglo-Dutch war and renamed to 'New York'.", "Gain a percentage of money each day."),
+    SPAIN(1,"Spain", "The very first settlement in the New World was built by Christopher Columbus in 1492 from the remains of the Spanish ship 'The Santa Mar�a', he called it 'La Navidad'. The settlement was on the island what is now known as Haiti. \nSpain went on to colonize the Caribbean Islands, Mexico and large parts of north and south America.", "Chance of extra drops while mining."),
+    FRANCE(0,"France", "In the early 17th century, french colonists founded Fort Royal in today's Nova Scotia, Canada. \nNew France was quickly founded as part of the French colonial empire with it's capital: Quebec. New France occupied large parts of North-East Canada and the USA's Midwest until Britain captured the land during several wars.", "Bonus resistance from armor.");
+
+    private int id;
+    private String title;
+    private String lore;
+    private String traits;
+
+    NationType(int id, String title, String lore, String traits){
+        this.id = id;
+        this.title = title;
+        this.lore = lore;
+        this.traits = traits;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+
+    public String getTraits(){
+        return traits;
+    }
+    
+    public String getLore(){
+        return lore;
+    }
+
+    public static boolean contains(String text){
+        text = text.toUpperCase();
+        for(NationType n: NationType.values()){
+            if(n.name().equals(text)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
