9012b8040be5860eb19906c042bcf27b1e476f24
Ionaru <Jeroen_akkerman1@hotmail.com>
2014-05-27 23:53:05 +0200

    Didn't commit for some odd reason...

---
+++ src/main/java/me/ionaru/nations/Nation.java
@@ -0,0 +1,55 @@
+package me.ionaru.nations;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Nation{
+
+    public static void addToNation(NationType type, Player player){
+        if(isInNation(player)){
+            player.sendMessage(ChatColor.RED + "You cannot join this nation because you are already in a nation.");
+            return;
+        }
+        player.sendMessage(Nations.colorize("&aYou have joined " + type.getTitle()));
+        Nations.nations.put(player.getName(), type);
+    }
+
+    public static boolean isInNation(Player player){
+        return Nations.nations.containsKey(player.getName());
+    }
+
+    public static void removeFromNation(Player player){
+        if(!isInNation(player)){
+            player.sendMessage(ChatColor.RED + "You cannot leave a nation because you are not in a nation.");
+            return;
+        }
+        Nations.nations.remove(player.getName());
+        player.sendMessage(ChatColor.GREEN + "You left your nation.");
+    }
+
+    public static void clearNations(){
+        Nations.nations.clear();
+    }
+
+    public static List<String> getAllPlayersInNations(){
+        List<String> players = new ArrayList<String>();
+        players.addAll(Nations.nations.keySet());
+        return players;
+    }
+
+    public static NationType getNationType(Player player){
+        if (!isInNation(player)){
+            return null;
+        }
+        return Nations.nations.get(player.getName());
+    }
+}
